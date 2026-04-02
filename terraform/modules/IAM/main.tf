360d9bc3ff9889af0c92e19853ade81e05522c36
David Newman <misterdnewman@icloud.com>
2026-04-01 18:06:05 -0700

    feat(mod/IAM): update repo access string

--- terraform/modules/IAM/main.tf
+++ terraform/modules/IAM/main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "github_actions_role" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:misterdnewman/cvs-platform-challenge:*"
+          "token.actions.githubusercontent.com:sub" = "repo:misterdnewman/cvs-platform-challenge:ref:refs/heads/main"
         }
       }
     }]
