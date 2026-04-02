8988c16c7f116b04b586721b2e1fd98c3016c815
David Newman <misterdnewman@icloud.com>
2026-04-01 19:03:26 -0700

    feat(mod/IAM): user name on repo was incorrect. :/

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -2,7 +2,7 @@
 module "events_table" {
   source       = "../modules/dynamodb"
   table_name   = "${var.project_name}-${var.environment}-events"
-  hash_key     = "event_id"   # ← THIS is the value
+  hash_key     = "event_id" # ← THIS is the value
   project_name = var.project_name
   environment  = var.environment
 }
--- terraform/modules/IAM/main.tf
+++ terraform/modules/IAM/main.tf
@@ -3,24 +3,25 @@
 resource "aws_iam_role" "github_actions_role" {
   name = "${var.project_name}-${var.environment}-github-actions-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = "arn:aws:iam::160823835275:oidc-provider/token.actions.githubusercontent.com"
+assume_role_policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [{
+    Effect = "Allow"
+    Principal = {
+      Federated = "arn:aws:iam::160823835275:oidc-provider/token.actions.githubusercontent.com"
+    }
+    Action = "sts:AssumeRoleWithWebIdentity"
+    Condition = {
+      StringLike = {
+        "token.actions.githubusercontent.com:sub" = "repo:mrdnewman/cvs-platform-challenge:*"
+      } 
+      StringEquals = {
+        "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
       }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-        }
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:misterdnewman/cvs-platform-challenge:ref:refs/heads/main"
-        }
-      }
-    }]
-  })
+   }
+  }]
+})
+
 }
 
 # Permissions for pipeline (minimal)
